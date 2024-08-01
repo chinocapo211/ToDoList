284adfcbf4ae3abfb678b34d569225d7d964ac69
FELIPE BLAUSTEIN <47807182@ort>
2024-08-01 11:54:53 -0300

    avance

--- App.js
+++ App.js
@@ -2,14 +2,18 @@ import 'react-native-gesture-handler';
 import React from 'react';
 import { NavigationContainer } from '@react-navigation/native';
 import { createStackNavigator } from '@react-navigation/stack';
+import Home from './src/screens/home';
+import Tareas from './src/screens/tareas';
+
 
 const Stack = createStackNavigator();
 
 export default function App() {
   return (
     <NavigationContainer>
-      <Stack.Navigator initialRouteName="Ajustes" screenOptions={{ headerShown: false }}>
+      <Stack.Navigator initialRouteName="Tareas" screenOptions={{ headerShown: false }}>
         <Stack.Screen name="Home" component={Home} />
+        <Stack.Screen name="Tareas" component={Tareas} />
       </Stack.Navigator>
     </NavigationContainer>
   );}
